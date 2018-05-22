2d33343345aa3df997b4cdc01f424fb2090900b6
ssgkmd <tlagyqls7@naver.com>
2018-05-22 16:35:20 +0900

    css good

--- miro/form.py
+++ miro/form.py
@@ -3,12 +3,12 @@ from django import forms
 from .models import User_data
 
 class UsersForm(forms.Form):
-    user_id = forms.CharField(label = 'ID',max_length=20)
-    user_pw = forms.CharField(label = 'PW',max_length=20,widget=forms.PasswordInput)
+    user_id = forms.CharField(label = 'ID',max_length=20, widget=forms.TextInput(attrs={'class' : 'charfieldClass', 'placeholder' : 'ID'}))
+    user_pw = forms.CharField(label = 'PW',max_length=20,widget=forms.PasswordInput(attrs={'class' : 'charfieldClass', 'placeholder' : 'PW'}))
     #model = User_data
     #fields = ['user_id','user_pw']
 
 class RegistForm(forms.Form):
-    user_id = forms.CharField(label = 'ID',max_length=20)
-    user_pw = forms.CharField(label = 'PW',max_length=20,widget=forms.PasswordInput)
-    user_name = forms.CharField(label='NAME',max_length=20)
+    user_id = forms.CharField(label = 'ID',max_length=20, widget=forms.TextInput(attrs={'class' : 'charfieldClass', 'placeholder' : 'ID'}))
+    user_pw = forms.CharField(label = 'PW',max_length=20,widget=forms.PasswordInput(attrs={'class' : 'charfieldClass', 'placeholder' : 'PW'}))
+    user_name = forms.CharField(label='NAME',max_length=20, widget=forms.TextInput(attrs={'class' : 'charfieldClass', 'placeholder' : 'NAME'}))
